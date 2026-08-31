fb25f3757d8a9023f080a5623589c2b08ce9eadc
sara <benaichasara359@gmail.com>
2026-08-31 18:24:59 +0200

    chande the first code

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-console.log("hello javascript")
+console.log("hello javascript");
+// Declare variables
+let name = "Alice";
+let age = 25;
+
+// Output to console
+console.log(`Hello, my name is ${name} and I am ${age} years old.`);
