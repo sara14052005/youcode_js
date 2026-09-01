581ca7018dfac671aa8fb5a839760187903e56fa
sara <benaichasara359@gmail.com>
2026-09-01 11:04:59 +0200

    get back to origin

--- hello.js
+++ hello.js
@@ -1,7 +1,2 @@
 console.log("hello javascript");
-// Declare variables
-let name = "Alice";
-let age = 25;
 
-// Output to console
-console.log(`Hello, my name is ${name} and I am ${age} years old.`);
