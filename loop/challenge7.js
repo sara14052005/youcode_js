3f7898bb0ed5431c39709cc045f9ef5977b30d44
sara <benaichasara359@gmail.com>
2026-09-02 17:32:14 +0100

    challenge loop_avance part2

--- loop/challenge7.js
+++ loop/challenge7.js
@@ -1,14 +1,10 @@
 const p = require('prompt-sync')();
 let n = Number(p("Enter un nobmre: ")); 
-/*var i=0;
+var i=0;
 while(n != 0){
     let r = n%10;
     if(Math.floor(n/10) == 0) i+=r;
     else i=(i+r)*10;
     n=Math.floor(n/10);
-}*/
-while(n != 0){
-    let r = n%10
-    process.stdout.write(`${r}`);
-    n=Math.floor(n/10);
 }
+process.stdout.write(`${i}`);
--- loop/challenge8.js
+++ loop/challenge8.js
@@ -1,15 +1,15 @@
 const p = require('prompt-sync')();
 let n = Number(p("Enter un nobmre: ")); 
-var i=2,f=0,f0=0,f1=1;
-while(i<=n){
-    if(n != 1 && n != 0){
+var i=0,f=0,f0=0,f1=1;
+while(i<n){
+    if(i != 1 && i != 0){
         f=f0+f1;
         f0=f1;
         f1=f;
+        console.log(`F(${i}) = ${f}`)
     }else{
-        if(n == 1) f=1;
-        else f=0;
+        if(i == 1) {f=1; console.log(`F(${i}) = ${f}`);}
+        else{f=0; console.log(`F(${i}) = ${f}`);} 
     }
     i++;
 }
-console.log(`F(${n}) = ${f}`)
