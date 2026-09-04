98937672c67a039ce9a24e8b667cb6e9801fafa1
sara <benaichasara359@gmail.com>
2026-09-04 08:58:19 +0100

    challenge tableaux

--- one_to_one.js
+++ one_to_one.js
@@ -1,8 +1,8 @@
-const taille =4;
 for(let i=1;i<=4;i++){
-    for(let j=1;j<=i;j++){
-        if(j%2 == 0) process.stdout.write(`[${j}]`);
-        else process.stdout.write(`${j}`);
-    }
-    console.log("")
+    for(let j=1;j<=4;j++)
+        {
+            if(j%2 == 0) process.stdout.write(`[${j}]`);
+            else process.stdout.write(`${j}`);
+        } 
+    console.log("");
 }
