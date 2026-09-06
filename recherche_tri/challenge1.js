d877a06f3e2661b13082ea281175808a3715d236
sara <benaichasara359@gmail.com>
2026-09-06 11:20:14 +0100

    presentation

--- recherche_tri/challenge1.js
+++ recherche_tri/challenge1.js
@@ -1,13 +1,16 @@
 const t=[45,13,2,8,96,17,23,78];
 console.log(t)
-for(let i=0;i<t.length;i++){
-    for(let j=0;j<t.length-i;j++){
+var d=t.length-2;
+console.log(d)
+for(let i=0;i<t.length-1;i++){
+    for(let j=0;j<=d;j++){
         if(t[j]>t[j+1]){
             let c=t[j];
             t[j]=t[j+1];
             t[j+1]=c;
         }
     } 
+    d--;
     
 }
 console.log(t)
